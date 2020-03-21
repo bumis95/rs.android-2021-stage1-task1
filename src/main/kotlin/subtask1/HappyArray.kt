9670a16321b927df710547feb8837c937641684a
bumis95 <bumis95@gmail.com>
2020-03-21 12:56:03 +0300

    Add HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,37 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
+    var needCycle = true
+
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        var result = sadArray
+        if (sadArray.isEmpty()) return result
+
+        while (needCycle) {
+            needCycle = false
+            result = cycle(result)
+        }
+
+        return result
+    }
+
+    fun cycle(copyOfSadArray: IntArray) : IntArray {
+
+        val happyArray = mutableListOf<Int>()
+        val sizeSadArray = copyOfSadArray.size
+        happyArray.add(copyOfSadArray[0])
+
+        for (index in copyOfSadArray.indices) {
+            when (index) {
+                in 1..(sizeSadArray - 2) ->
+                    if (copyOfSadArray[index] < (copyOfSadArray[index - 1] + copyOfSadArray[index + 1])) {
+                        happyArray.add(copyOfSadArray[index])
+                    }
+                    else needCycle = true
+            }
+        }
+        happyArray.add(copyOfSadArray[sizeSadArray-1])
+        return happyArray.toIntArray()
     }
 }
