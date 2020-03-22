47fc19ff2666ec5ea87d96798bbb07f778f67a01
bumis95 <bumis95@gmail.com>
2020-03-22 20:23:02 +0300

    Add StringParser

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,37 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
+    private fun findCloseBracket(char: Char) = when (char) {
+        '(' -> ')'
+        '[' -> ']'
+        else -> '>'
+    }
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        val list = mutableListOf<String>()
+        var closeBracket: Char
+        var counter = 0
+
+        inputString.forEachIndexed { index, char ->
+
+            if (char == '<' || char == '[' || char == '(') {
+                closeBracket = findCloseBracket(char)
+                loop@ for (innerIndex in (index + 1)..inputString.length) {
+                    when(inputString[innerIndex]) {
+                        char -> counter++
+                        closeBracket -> {
+                            if (counter == 0) {
+                                list.add(inputString.substring(index + 1, innerIndex))
+                                break@loop
+                            }
+                            else counter--
+                        }
+                    }
+                }
+            }
+        }
+//        println(list.joinToString())
+        return list.toTypedArray()
     }
 }
